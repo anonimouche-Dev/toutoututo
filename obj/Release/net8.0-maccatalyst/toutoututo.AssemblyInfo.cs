0d3290b01affd38551281d4b7414f79f4bdedf26
anonimouche-Dev <144521481+anonimouche-Dev@users.noreply.github.com>
2024-08-17 21:53:46 +0200

    toutoututopdtate

--- obj/Release/net8.0-maccatalyst/toutoututo.AssemblyInfo.cs
+++ obj/Release/net8.0-maccatalyst/toutoututo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("toutoututo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f74f88be8ce11f7fbfeb0804b1ca8e38875a5766")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+192d5bbd8319b280852bfa2dd7975f0e2dc185c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("toutoututo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("toutoututo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
